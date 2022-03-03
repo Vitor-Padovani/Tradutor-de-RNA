63051d809703a62576f6a92ca88ed57d938dfe55
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2022-03-03 08:38:30 -0300

    cdn added

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import functions as f
 
-input_file = open('input/input.txt', 'r')
-output_file = open('output/output.txt', 'w')
-
 
 while True:
-    
     codons = ''
-    codons = f.format_str(input_file.read())
-
-    data = f.convert_codons(codons)
 
     #print(data)
 
-    option = input('option: ')
+    f.line(45)
+    print("'all' - tudo | 'ptn' - proteínas | 'cdn' - códons | ' ' - sair")
+    f.line(45)
+    option = input('comando: ').strip()
+    print()
     if option == '':
         break
+
+    input_file = open('input/input.txt', 'r')
+    output_file = open('output/output.txt', 'w')
+    codons = f.format_str(input_file.read())
+    data = f.convert_codons(codons)
+
 
     match option:
         case 'all':
@@ -54,7 +57,27 @@ while True:
                     ptn_format = 1
                 
             output_file.close()
-
+            print()
+        
+        case 'cdn':
+            cdn_num = 0
+            cdn_format = 1
+            for i in range(len(data[0])):
+                if data[1][i][0] != 'Stop':
+                    if cdn_format % 10 == 0:
+                        print(f'{data[0][i]}')
+                        output_file.writelines(f'{data[0][i]}\n')
+                    else:
+                        print(f'{data[0][i]}', end=' - ')
+                        output_file.writelines(f'{data[0][i]} - ')
+                    cdn_format += 1
+                else:
+                    cdn_num += 1
+                    print(f'STOP [Proteína {cdn_num}]\n')
+                    output_file.writelines(f'STOP [Proteína {cdn_num}]\n\n')
+                    cdn_format = 1
+                
+            output_file.close()
             print()
 
     #print(data)
